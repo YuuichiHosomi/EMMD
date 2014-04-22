f99fb31f9e067c80ed9820a71b438b9c98d09771
electroYas <yas.nimal@gmail.com>
2014-04-23 00:43:47 +0700

    Fourth Commit

--- control/control.ino
+++ control/control.ino
@@ -1,13 +1,8 @@
 #include <math.h>
 float p[20][2];
-float beta=0;
-float theta=0;
-float alpha=0;
 int pos;
 int max_pos;
 float distance;
-float current_dir;
-float current_pos[2];
 float Kp;
 float rudder_angle;
 
@@ -26,52 +21,48 @@ void loop()
   // put your main code here, to run repeatedly:
   while(pos<max_pos){
     
-    
-    //getting current position
-    current_pos[0] = get_X_GPS();
-    current_pos[1] = get_Y_GPS();
-    
-    distance = sqrt(pow(p[pos+1][0]-current_pos[0],2)+pow(p[pos+1][1]-current_pos[1],2));    
- 
-    //calculate beta
-    current_dir = get_Dir();
-    beta = current_dir;
-    //calculate alpha
-    alpha=atan((p[pos+1][1]-current_pos[1])/(p[pos+1][0]-current_pos[0]));// atan((y1-y0)/(x1-x0))
-    //calculate theta
-    theta = alpha-beta; 
-    //control algorithm
-    rudder_angle = Kp * theta;
+    rudder_angle = control(p[pos+1][0],p[pos+1][1]); //float control(float dest_x,float dest_y)
+   
     if(distance < 5){
       pos++;
     }
   }
   
   //gohome
-  while(distance<1){
-    
+  while(distance>1){
+    rudder_angle = control(p[0][0],p[0][1]);
+   
+  }
+  
+  while(true){}
+ 
+}
+
+float control(float dest_x,float dest_y){
+  
+  float current_pos[2];
+  float current_dir;
+  float beta;
+  float alpha;
+  float theta;
+  float rudder;
+  
+  //getting current position
     current_pos[0] = get_X_GPS();
     current_pos[1] = get_Y_GPS();
     
-    distance = sqrt(pow(p[0][0]-current_pos[0],2)+pow(p[0][1]-current_pos[1],2));    
+    distance = sqrt(pow(dest_x-current_pos[0],2)+pow(dest_y-current_pos[1],2));    
  
     //calculate beta
     current_dir = get_Dir();
     beta = current_dir;
     //calculate alpha
-    alpha=atan((p[0][1]-current_pos[1])/(p[0][0]-current_pos[0]));// atan((y1-y0)/(x1-x0))
+    alpha=atan((dest_y-current_pos[1])/(dest_x-current_pos[0]));// atan((y1-y0)/(x1-x0))
     //calculate theta
     theta = alpha-beta; 
     //control algorithm
-    rudder_angle = Kp * theta; //proportinal control
-    if(distance < 5){
-      pos++;
-    }
-    
-  }
-  
-  
-  
+    rudder = Kp * theta;  
+    return rudder;
 }
 
 float get_X_GPS(){
